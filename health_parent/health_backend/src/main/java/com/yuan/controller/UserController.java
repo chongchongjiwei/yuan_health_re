d7f4e442c95ac1950283f90daaf55b16e27f171e
yuan <yuan@qq.com>
2023-01-30 09:59:05 +0800

    完成权限配置，并显示用户名在主页。

---
+++ health_parent/health_backend/src/main/java/com/yuan/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.yuan.controller;
+
+import com.yuan.constant.MessageConstant;
+import com.yuan.entity.Result;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @RequestMapping("/getUsername")
+    public Result getUsername()throws Exception{
+        try{
+            org.springframework.security.core.userdetails.User user =
+                    (org.springframework.security.core.userdetails.User)
+                            SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            return new Result(true, MessageConstant.GET_USERNAME_SUCCESS,user.getUsername());
+        }catch (Exception e){
+            return new Result(false, MessageConstant.GET_USERNAME_FAIL);
+        }
+    }
+}
